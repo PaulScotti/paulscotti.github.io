dc8e60fbb2ec42e5eac0d98854c9abc15b6ab94b
PaulScotti <scottibrain@gmail.com>
2018-03-06 09:28:10 -0500

    34gg43

--- mturk/Cont_LTM_101_files/colors.js
+++ mturk/Cont_LTM_101_files/colors.js
@@ -360,20 +360,38 @@ colors = [
 			[245,37,114],
 			[245.5,37,112.5]
 		];
+
 dx = {
-        "repeatimg": [101,102,103,104,105,106,107,108,109,110,111,112],
-        "r_repeatimg": [101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202],
-        "pracimg": [131,132,133,134],
-        "r_study":[136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,231,232,233,234,235,236,237,238],
-        "repeatprac":[],
-        "repeatpracimg":[],
-        "r_TrialType":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
-        "rep_TrialType":[0,0,0,0,0,0,1,1,2,2,3,3]
+        "repeatimg": counter(101,totalRepeats+101-1),
+        "pracimg": counter(131,numTrialsPerPart[0]+131-1),
+        "r_study": counter(136,numTrialsPerPart[1]+136-1),
+				"r_repeatimg": counter(101,numTrialsPerPart[1]+101-1),
+				"r_TrialType": repmat([0,0,0,1,2,3],numTrialsPerPart[1]/6),
+				"rep_TrialType": repmat([0,0,0,1,2,3],totalRepeats/6)
 	};
+
+	function repmat(array, count) {
+	    var result = [];
+	    while (count--) {
+	        result = result.concat(array);
+	    }
+	    return result;
+	}
+	function counter(start, end, interval) {
+			if (interval == undefined) { var interval = 1; }
+			var result = [];
+			while (start!=end+interval) {
+					result = result.concat(start);
+					start = start+interval;
+			}
+			return result;
+	}
+
 /* 12 repeats, 102 test objects, 50% rich quad (1)
 TrialType is for test objects
 r_TrialType is for the repeat-detection objects
+rep_TrialType is the seeds used for repeated objects
 r_repeatimg determines what trial to repeat on
 repeatimg are the IDs for the repeated images
-r_study are the presented objects that will be tested 
+r_study are the presented objects that will be tested
 colors is for CIE LAB RGB conversions */
