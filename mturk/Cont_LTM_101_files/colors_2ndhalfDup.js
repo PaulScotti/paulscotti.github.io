f39aa068f0fc081d528930fbe129b5058d1a0971
PaulScotti <scottibrain@gmail.com>
2018-05-10 08:40:47 -0400

    aweg

--- mturk/Cont_LTM_101_files/colors_2ndhalfDup.js
+++ mturk/Cont_LTM_101_files/colors_2ndhalfDup.js
@@ -365,7 +365,7 @@ let colors = [
         "repeatimg": counter(101,101+totalRepeats-1),
         "pracimg": counter(101+totalRepeats,101+totalRepeats+numTrialsPerPart[0]-1),
         "r_study": counter(101+totalRepeats+numTrialsPerPart[0],101+totalRepeats+numTrialsPerPart[0]+numTrialsPerPart[1]-1),
-				"r_repeattrial": counter(1,numTrialsPerPart[1],2),
+				"r_repeattrial": counter(1,actualStudyNum,2),
 				"r_TrialType": repmat([0,0,1,2,3],trialsPerBlock/5),
 				"rep_TrialType": repmat([0,0,1,2,3],Math.ceil(totalRepeats/5))
 	};
