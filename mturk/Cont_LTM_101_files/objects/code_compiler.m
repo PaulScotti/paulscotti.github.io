960c0cbebb8c74fa9e54a58b64e6ef1179210c89
PaulScotti <scottibrain@gmail.com>
2018-05-10 16:47:24 -0400

    wh45

--- mturk/Cont_LTM_101_files/objects/code_compiler.m
+++ mturk/Cont_LTM_101_files/objects/code_compiler.m
@@ -3,7 +3,7 @@ clc; clear;
 code = [];
 preset = ['<img id="IDN" src="https://paulscotti.github.io/mturk/Cont_LTM_101_files/objects/Object.png" style="margin-left:-1000px">'];
 
-for i = 400:400
+for i = 401:405
     presetNew = strrep(preset,'Object',['Object',num2str(100+i)]);
     presetNew = strrep(presetNew,'IDN',[num2str(100+i)]);
     code = strcat(code,presetNew);
