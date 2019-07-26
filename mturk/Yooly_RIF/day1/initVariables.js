259fe3cd8ef0d5bd67e37e21599b6956f6806ac3
PaulScotti <scottibrain@gmail.com>
2019-07-26 14:12:59 -0400

    a

--- mturk/Yooly_RIF/day1/initVariables.js
+++ mturk/Yooly_RIF/day1/initVariables.js
@@ -29,9 +29,9 @@ for (var i = 0; i < Triplets.length; i++) {
     StudyArray.push("d2"+Triplets[i]); bb++
   } else if (i==0){
     StudyArray.push("d1"+Triplets[i]); ee++
-  } else if (Triplets[i].includes("o") && Triplets[i-1].includes("plus")) {
+  } else if (Triplets[i].includes("o") && Triplets[i-1].includes("plus")) {A3U2GAVNJBSKL3
     StudyArray.push("pos3minus"+Triplets[i]); dd++
-  } else if (i == Triplets.length) {
+  } else if (i == Triplets.length-1) {
     StudyArray.push("d1"+Triplets[i]); ee++
   } else if (Triplets[i].includes("o") && Triplets[i+1].includes("plus")) {
     StudyArray.push("pos1minus"+Triplets[i]); cc++
